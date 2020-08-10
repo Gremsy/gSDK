7e408c3f51657deb0c51d11addef2c869d997cbd
Chu Nguyen <44431095+g-chunguyen@users.noreply.github.com>
2020-08-10 15:05:14 +0700

    Update gimbal_interface.h
    add Gremsy's Lisence

--- gimbal_interface.h
+++ gimbal_interface.h
@@ -1,16 +1,32 @@
 /*******************************************************************************
- * Copyright (c) 2018, The GremsyCo
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification,
- * are strictly prohibited without prior permission of The GremsyCo.
- *
  * @file    gimbal_interface.h
  * @author  The GremsyCo
- * @version V1.0.0
- * @date    August-021-2018
- * @brief   This file contains expand of gMavlink
+ * @version V2.3.0
+ * @date    August-21-2018
+ * @brief   This file contains API for gimbal interface
+ *
+ *  @Copyright (c) 2018 Gremsy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  ******************************************************************************/
+
 /* Includes ------------------------------------------------------------------*/
 #ifndef GIMBAL_INTERFACE_H_
 #define GIMBAL_INTERFACE_H_
@@ -822,4 +838,4 @@ private:
 
 #endif // GIMBAL_INTERFACE_H_
 
-/*********** Portions COPYRIGHT 2018 Gremsy.Co., Ltd.*****END OF FILE**********/
+/************************ (C) COPYRIGHT Gremsy *****END OF FILE****************/
