e5d026fbce8d25f0d193aebf502c3fe656e87489
Chu Nguyen <44431095+g-chunguyen@users.noreply.github.com>
2020-08-10 15:05:47 +0700

    Update mavlink_control.cpp
    Add Gremsy's License

--- mavlink_control.cpp
+++ mavlink_control.cpp
@@ -1,14 +1,30 @@
 /*******************************************************************************
- * Copyright (c) 2018, The GremsyCo
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification,
- * are strictly prohibited without prior permission of The GremsyCo.
- *
- * @file    mavlink_control.c
+ * @file    mavlink_control.cpp
  * @author  The GremsyCo
- * @version V1.0.0
- * @date    August-021-2018
- * @brief   This file contains example how to control gimbal with API
+ * @version V2.3.0
+ * @date    August-21-2018
+ * @brief   This file contains a example for showing how to control gimbal in
+ *          some cases
+ *
+ *  @Copyright (c) 2018 Gremsy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  ******************************************************************************/
  
@@ -699,4 +715,4 @@ main(int argc, char **argv)
 }
 
 
-/*********** Portions COPYRIGHT 2018 Gremsy.Co., Ltd.*****END OF FILE**********/
+/************************ (C) COPYRIGHT Gremsy *****END OF FILE****************/
